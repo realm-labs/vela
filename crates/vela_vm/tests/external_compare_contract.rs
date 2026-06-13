2641f9c2682583be1e546771b50985c219b9a9c9
mikai <dreamfever2017@yahoo.com>
2026-06-13 14:05:37 +0800

    test(vm): add scalar opcode visibility report

--- crates/vela_vm/tests/external_compare_contract.rs
+++ crates/vela_vm/tests/external_compare_contract.rs
@@ -3,11 +3,13 @@ mod version;
 #[path = "../benches/external_compare/workloads.rs"]
 mod workloads;
 
-use std::collections::BTreeSet;
+use std::collections::{BTreeMap, BTreeSet};
 
 use mlua::{Function, Lua};
 use rhai::{Engine, Scope};
 use vela_bytecode::compiler::compile_program_source_with_registry;
+use vela_bytecode::linked::InstructionKind;
+use vela_bytecode::{BinaryLiteralOp, BinaryLiteralSide};
 use vela_bytecode::{Linker, UnlinkedProgram};
 use vela_common::ScalarValue;
 use vela_common::SourceId;
@@ -71,6 +73,35 @@ fn external_compare_vela_sources_compile_and_link() {
         link_program_for_vm(&vm, &program)
             .unwrap_or_else(|error| panic!("{} should link: {error}", workload.name));
     }
+}
+
+#[test]
+fn scalar_workloads_have_reproducible_opcode_count_reports() {
+    let vm = Vm::new().with_standard_natives();
+    let registry = vela_stdlib::standard_registry().expect("standard registry should build");
+
+    let scalar = opcode_count_report(&vm, registry.compile_view(), "scalar_branch_loop");
+    assert_has_opcode(&scalar, "BinaryIntLiteral::Rem::Right");
+    assert_has_opcode(&scalar, "Mul");
+    assert_has_opcode(&scalar, "Greater");
+    assert_has_opcode(&scalar, "Equal");
+    assert_has_opcode(&scalar, "Add");
+    assert_has_opcode(&scalar, "JumpIfFalse");
+    assert_has_opcode(&scalar, "Jump");
+    assert_has_opcode(&scalar, "RangeNext");
+
+    let range = opcode_count_report(&vm, registry.compile_view(), "range_iteration");
+    assert_has_opcode(&range, "RangeNext");
+    assert_has_opcode(&range, "Add");
+    assert_has_opcode(&range, "Sub");
+
+    let function_calls = opcode_count_report(&vm, registry.compile_view(), "function_calls");
+    assert_has_opcode(&function_calls, "CallFunction");
+
+    let float_math = opcode_count_report(&vm, registry.compile_view(), "float_math_loop");
+    assert_has_opcode(&float_math, "BinaryFloatLiteral::Add::Right");
+    assert_has_opcode(&float_math, "Mul");
+    assert_has_opcode(&float_math, "Div");
 }
 
 #[test]
@@ -200,4 +231,153 @@ fn link_program_for_vm(
     linker
         .link_program(program)
         .map_err(|error| format!("{error:?}").into())
+}
+
+fn opcode_count_report(
+    vm: &Vm,
+    registry: vela_registry::RegistryCompileView<'_>,
+    workload_name: &str,
+) -> BTreeMap<&'static str, usize> {
+    let workload = workloads::all_workloads()
+        .find(|workload| workload.name == workload_name)
+        .unwrap_or_else(|| panic!("{workload_name} workload should exist"));
+    let program = compile_program_source_with_registry(SourceId::new(1), workload.vela, registry)
+        .unwrap_or_else(|error| panic!("{workload_name} should compile: {error:?}"));
+    let linked = link_program_for_vm(vm, &program)
+        .unwrap_or_else(|error| panic!("{workload_name} should link: {error}"));
+    let mut counts = BTreeMap::new();
+    for (_, function) in linked.functions() {
+        for instruction in &function.instructions {
+            *counts.entry(opcode_label(&instruction.kind)).or_insert(0) += 1;
+        }
+    }
+    counts
+}
+
+fn assert_has_opcode(counts: &BTreeMap<&'static str, usize>, opcode: &'static str) {
+    assert!(
+        counts.get(opcode).copied().unwrap_or_default() > 0,
+        "expected {opcode} in opcode count report: {counts:#?}"
+    );
+}
+
+fn opcode_label(kind: &InstructionKind) -> &'static str {
+    match kind {
+        InstructionKind::LoadConst { .. } => "LoadConst",
+        InstructionKind::Move { .. } => "Move",
+        InstructionKind::Not { .. } => "Not",
+        InstructionKind::Truthy { .. } => "Truthy",
+        InstructionKind::Negate { .. } => "Negate",
+        InstructionKind::Add { .. } => "Add",
+        InstructionKind::Sub { .. } => "Sub",
+        InstructionKind::Mul { .. } => "Mul",
+        InstructionKind::Div { .. } => "Div",
+        InstructionKind::Rem { .. } => "Rem",
+        InstructionKind::Equal { .. } => "Equal",
+        InstructionKind::NotEqual { .. } => "NotEqual",
+        InstructionKind::Less { .. } => "Less",
+        InstructionKind::LessEqual { .. } => "LessEqual",
+        InstructionKind::Greater { .. } => "Greater",
+        InstructionKind::GreaterEqual { .. } => "GreaterEqual",
+        InstructionKind::BinaryIntLiteral { op, side, .. } => binary_int_literal_label(*op, *side),
+        InstructionKind::BinaryFloatLiteral { op, side, .. } => {
+            binary_float_literal_label(*op, *side)
+        }
+        InstructionKind::GuardType { .. } => "GuardType",
+        InstructionKind::JumpIfFalse { .. } => "JumpIfFalse",
+        InstructionKind::JumpIfNotMissing { .. } => "JumpIfNotMissing",
+        InstructionKind::Jump { .. } => "Jump",
+        InstructionKind::CallNative { .. } => "CallNative",
+        InstructionKind::CallFunction { .. } => "CallFunction",
+        InstructionKind::MakeClosure { .. } => "MakeClosure",
+        InstructionKind::CallClosure { .. } => "CallClosure",
+        InstructionKind::CallMethod { .. } => "CallMethod",
+        InstructionKind::CallDynamicMethod { .. } => "CallDynamicMethod",
+        InstructionKind::TryPropagate { .. } => "TryPropagate",
+        InstructionKind::MakeArray { .. } => "MakeArray",
+        InstructionKind::MakeMap { .. } => "MakeMap",
+        InstructionKind::MakeRange { .. } => "MakeRange",
+        InstructionKind::MakeRecord { .. } => "MakeRecord",
+        InstructionKind::MakeEnum { .. } => "MakeEnum",
+        InstructionKind::GetRecordSlot { .. } => "GetRecordSlot",
+        InstructionKind::SetRecordSlot { .. } => "SetRecordSlot",
+        InstructionKind::GetEnumSlot { .. } => "GetEnumSlot",
+        InstructionKind::GetIndex { .. } => "GetIndex",
+        InstructionKind::SetIndex { .. } => "SetIndex",
+        InstructionKind::IterInit { .. } => "IterInit",
+        InstructionKind::IterNext { .. } => "IterNext",
+        InstructionKind::RangeNext { .. } => "RangeNext",
+        InstructionKind::EnumTagEqual { .. } => "EnumTagEqual",
+        InstructionKind::LoadGlobal { .. } => "LoadGlobal",
+        InstructionKind::HostRead { .. } => "HostRead",
+        InstructionKind::HostWrite { .. } => "HostWrite",
+        InstructionKind::HostMutate { .. } => "HostMutate",
+        InstructionKind::HostRemove { .. } => "HostRemove",
+        InstructionKind::HostCall { .. } => "HostCall",
+        InstructionKind::Return { .. } => "Return",
+    }
+}
+
+fn binary_int_literal_label(op: BinaryLiteralOp, side: BinaryLiteralSide) -> &'static str {
+    match (op, side) {
+        (BinaryLiteralOp::Add, BinaryLiteralSide::Left) => "BinaryIntLiteral::Add::Left",
+        (BinaryLiteralOp::Add, BinaryLiteralSide::Right) => "BinaryIntLiteral::Add::Right",
+        (BinaryLiteralOp::Sub, BinaryLiteralSide::Left) => "BinaryIntLiteral::Sub::Left",
+        (BinaryLiteralOp::Sub, BinaryLiteralSide::Right) => "BinaryIntLiteral::Sub::Right",
+        (BinaryLiteralOp::Mul, BinaryLiteralSide::Left) => "BinaryIntLiteral::Mul::Left",
+        (BinaryLiteralOp::Mul, BinaryLiteralSide::Right) => "BinaryIntLiteral::Mul::Right",
+        (BinaryLiteralOp::Div, BinaryLiteralSide::Left) => "BinaryIntLiteral::Div::Left",
+        (BinaryLiteralOp::Div, BinaryLiteralSide::Right) => "BinaryIntLiteral::Div::Right",
+        (BinaryLiteralOp::Rem, BinaryLiteralSide::Left) => "BinaryIntLiteral::Rem::Left",
+        (BinaryLiteralOp::Rem, BinaryLiteralSide::Right) => "BinaryIntLiteral::Rem::Right",
+        (BinaryLiteralOp::Less, BinaryLiteralSide::Left) => "BinaryIntLiteral::Less::Left",
+        (BinaryLiteralOp::Less, BinaryLiteralSide::Right) => "BinaryIntLiteral::Less::Right",
+        (BinaryLiteralOp::LessEqual, BinaryLiteralSide::Left) => {
+            "BinaryIntLiteral::LessEqual::Left"
+        }
+        (BinaryLiteralOp::LessEqual, BinaryLiteralSide::Right) => {
+            "BinaryIntLiteral::LessEqual::Right"
+        }
+        (BinaryLiteralOp::Greater, BinaryLiteralSide::Left) => "BinaryIntLiteral::Greater::Left",
+        (BinaryLiteralOp::Greater, BinaryLiteralSide::Right) => "BinaryIntLiteral::Greater::Right",
+        (BinaryLiteralOp::GreaterEqual, BinaryLiteralSide::Left) => {
+            "BinaryIntLiteral::GreaterEqual::Left"
+        }
+        (BinaryLiteralOp::GreaterEqual, BinaryLiteralSide::Right) => {
+            "BinaryIntLiteral::GreaterEqual::Right"
+        }
+    }
+}
+
+fn binary_float_literal_label(op: BinaryLiteralOp, side: BinaryLiteralSide) -> &'static str {
+    match (op, side) {
+        (BinaryLiteralOp::Add, BinaryLiteralSide::Left) => "BinaryFloatLiteral::Add::Left",
+        (BinaryLiteralOp::Add, BinaryLiteralSide::Right) => "BinaryFloatLiteral::Add::Right",
+        (BinaryLiteralOp::Sub, BinaryLiteralSide::Left) => "BinaryFloatLiteral::Sub::Left",
+        (BinaryLiteralOp::Sub, BinaryLiteralSide::Right) => "BinaryFloatLiteral::Sub::Right",
+        (BinaryLiteralOp::Mul, BinaryLiteralSide::Left) => "BinaryFloatLiteral::Mul::Left",
+        (BinaryLiteralOp::Mul, BinaryLiteralSide::Right) => "BinaryFloatLiteral::Mul::Right",
+        (BinaryLiteralOp::Div, BinaryLiteralSide::Left) => "BinaryFloatLiteral::Div::Left",
+        (BinaryLiteralOp::Div, BinaryLiteralSide::Right) => "BinaryFloatLiteral::Div::Right",
+        (BinaryLiteralOp::Rem, BinaryLiteralSide::Left) => "BinaryFloatLiteral::Rem::Left",
+        (BinaryLiteralOp::Rem, BinaryLiteralSide::Right) => "BinaryFloatLiteral::Rem::Right",
+        (BinaryLiteralOp::Less, BinaryLiteralSide::Left) => "BinaryFloatLiteral::Less::Left",
+        (BinaryLiteralOp::Less, BinaryLiteralSide::Right) => "BinaryFloatLiteral::Less::Right",
+        (BinaryLiteralOp::LessEqual, BinaryLiteralSide::Left) => {
+            "BinaryFloatLiteral::LessEqual::Left"
+        }
+        (BinaryLiteralOp::LessEqual, BinaryLiteralSide::Right) => {
+            "BinaryFloatLiteral::LessEqual::Right"
+        }
+        (BinaryLiteralOp::Greater, BinaryLiteralSide::Left) => "BinaryFloatLiteral::Greater::Left",
+        (BinaryLiteralOp::Greater, BinaryLiteralSide::Right) => {
+            "BinaryFloatLiteral::Greater::Right"
+        }
+        (BinaryLiteralOp::GreaterEqual, BinaryLiteralSide::Left) => {
+            "BinaryFloatLiteral::GreaterEqual::Left"
+        }
+        (BinaryLiteralOp::GreaterEqual, BinaryLiteralSide::Right) => {
+            "BinaryFloatLiteral::GreaterEqual::Right"
+        }
+    }
 }
