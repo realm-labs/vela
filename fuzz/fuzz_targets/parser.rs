bab769276047230db854de343aa47f535a2b741b
mikai <dreamfever2017@yahoo.com>
2026-07-12 16:42:54 +0800

    fix(fuzz): use current syntax parser entrypoint

--- fuzz/fuzz_targets/parser.rs
+++ fuzz/fuzz_targets/parser.rs
@@ -2,9 +2,9 @@
 
 use libfuzzer_sys::fuzz_target;
 use vela_common::SourceId;
-use vela_syntax::parser::parse_source;
+use vela_syntax::parse::parse_source_with_id;
 
 fuzz_target!(|source: &str| {
-    let parsed = parse_source(SourceId::new(1), source);
+    let parsed = parse_source_with_id(SourceId::new(1), source);
     drop(parsed);
 });
